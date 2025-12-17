4f16298f20d95b4c7ee270d609dc7438c2e380a8
ptylczynski <ptylczynski@man.poznan.pl>
2025-12-17 21:43:01 +0100

    add: gpio.py

---
+++ gpio.py
@@ -0,0 +1,31 @@
+from gpiozero import LED
+
+import objects
+
+gpio_state = dict()
+
+def _create_gpio(pin_no: int):
+    try:
+        if pin_no not in gpio_state:
+            gpio_state[pin_no] = LED(pin_no, active_high=False)
+    except Exception as e:
+        print(e)
+        print("GPIO is disabled now!")
+        return None
+
+def _update_gpio():
+    for l in objects.create_or_load_lights():
+        pin_no = l.pin_no
+        state = l.state
+        name = l.name
+        _create_gpio(pin_no)
+        print(f"Setting pin {pin_no} to {state}")
+        if pin_no in gpio_state and gpio_state[pin_no]:
+            if state:
+                gpio_state[pin_no].on()
+            elif not state:
+                gpio_state[pin_no].off()
+        else:
+            print(f"No GPIO is set! Not changing state of light {name}")
+
+_update_gpio()
